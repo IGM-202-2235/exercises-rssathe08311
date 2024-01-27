44b2496358d8623b881e6f7428fb32889934e311
Reva <111722589+rssathe08311@users.noreply.github.com>
2024-01-27 15:52:47 -0500

    Finished Exercise 2

--- Exercise 1 - Geo Art/Assets/Scripts/CreatureSpawner.cs
+++ Exercise 1 - Geo Art/Assets/Scripts/CreatureSpawner.cs
@@ -1,13 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CreatureSpawner : MonoBehaviour
 {
+    [SerializeField]
+    GameObject creaturePrefab;
+
+    [SerializeField]
+    Vector3 spawnLoc1 = new Vector3(0f, 0f, 0f);
+    [SerializeField]
+    Vector3 spawnLoc2 = new Vector3(2f, 2f, 0f);
+    [SerializeField]
+    Vector3 spawnLoc3 = new Vector3(-2f, -2f, 0f);
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Instantiate(creaturePrefab, spawnLoc1, Quaternion.identity);
+        Instantiate(creaturePrefab, spawnLoc2, Quaternion.identity);
+        Instantiate(creaturePrefab, spawnLoc3, Quaternion.identity);
     }
 
     // Update is called once per frame
