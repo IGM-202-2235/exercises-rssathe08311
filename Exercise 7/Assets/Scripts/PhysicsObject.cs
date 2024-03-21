84e9e1c1525bf17ef61842fe4b21f8b092e27960
Reva <revassathe@gmail.com>
2024-03-21 10:42:46 -0400

    work started on Exercise 7

--- Exercise 7/Assets/Scripts/PhysicsObject.cs
+++ Exercise 7/Assets/Scripts/PhysicsObject.cs
@@ -8,17 +8,114 @@ public class PhysicsObject : MonoBehaviour
     private Vector3 direction;
     private Vector3 velocity;
     private Vector3 acceleration;
-    public float mass; 
+
+    public Vector3 Direction
+    {
+        get
+        {
+            return direction;
+        }
+    }
+
+    [SerializeField]
+    public float mass, maxSpeed;
+
+    //things that might go away
+    public bool useFriction, useGravity;
+
+    [SerializeField]
+    float frictionCoeef = 0;
+
+    [SerializeField] Camera cameraObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        position = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (useFriction)
+        {
+            ApplyFriction(frictionCoeef);
+        }
+
+        if (useGravity)
+        {
+            ApplyGravity(Vector3.down * 9.8f);
+        }
+        else
+        {
+            velocity = Vector3.zero;
+        }
+
+
+        //Calculate the velocity for this fram 
+        velocity += acceleration * Time.deltaTime;
+
+        Bounce();
+
+        position += velocity * Time.deltaTime;
+
+        //Grab current direction from velocity
+        direction = velocity.normalized;
+
+        transform.position = position;
+
+        //Zero out acceleration
+        acceleration = Vector3.zero;
+
+    }
+
+    //Force Methods
+    public void ApplyForce(Vector3 force)
+    {
+        acceleration += force / mass;
+    }
+
+
+    //coeff must be above 0 or else it will just cause the object to accelerate 
+    public void ApplyFriction(float coeff)
+    {
+        Vector3 friction = velocity * -1;
+        friction.Normalize();
+
+        friction = friction * coeff;
+
+        ApplyForce(friction);
+    }
+
+    public void ApplyGravity(Vector3 gravForce)
+    {
+        acceleration += gravForce;
+    }
+
+    void Bounce()
+    {
+        float totalCamHeight = (cameraObject.orthographicSize * 2f) / 2;
+        float totalCamWidth = (totalCamHeight * cameraObject.aspect);
+
+        if (position.y >= totalCamHeight) 
+        {
+            velocity.y *= -1;
+            //acceleration.y *= -1;
+        }
+        if(position.y <= (totalCamHeight * -1)){
+            velocity.y *= -1;
+            //acceleration.y *= -1;
+        }
+        if (position.x >= totalCamWidth)
+        {
+            velocity.x *= -1;
+            //acceleration.x *= -1;
+        }
+        if (position.x <= (totalCamWidth * -1))
+        {
+            velocity.x *= -1;
+            //acceleration.x *= -1;
+            //position.x = 
+        }
     }
 }
